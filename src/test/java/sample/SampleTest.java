964cd3cc025332ac531db3e4fa485ed2dab29064
金子拓己（本名） <takumise089@gmail.com>
2024-05-21 15:40:51 +0900

    add junit test

---
+++ src/test/java/sample/SampleTest.java
@@ -0,0 +1,13 @@
+package sample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+    @Test
+    public void test1() {
+        int i = 0;
+        assertEquals(i, 0);
+    }
+}
